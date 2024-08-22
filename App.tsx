6f1b0e4eb666ea952fa54edd7af6a9a2ed54cca5
jromero <jonathan.joseromero@gmail.com>
2024-08-21 23:32:41 -0400

    added navigation between the main screens

--- App.tsx
+++ App.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import ArticleListScreen from './src/screens/ArticleListScreen';
+import SingleArticleScreen from './src/screens/SingleArticleScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="ListScreen">
+        <Stack.Screen name="ListScreen" component={ArticleListScreen} />
+        <Stack.Screen name="SingleArticle" component={SingleArticleScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
