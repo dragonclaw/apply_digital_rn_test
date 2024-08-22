1c23733b7e16d6adbf3906f6466b5b9d418ab715
jromero <jonathan.joseromero@gmail.com>
2024-08-21 23:07:57 -0400

    first commit with react navigation

---
+++ src/screens/ArticleListScreen.tsx
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const ArticleListScreen = () => {
+  return (
+    <View>
+      <Text>ArticleScreen</Text>
+    </View>
+  );
+};
+
+export default ArticleListScreen;
+
+const styles = StyleSheet.create({});
---
+++ src/screens/SingleArticleScreen.tsx
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const SingleArticleScreen = () => {
+  return (
+    <View>
+      <Text>SingleArticleScreen</Text>
+    </View>
+  );
+};
+
+export default SingleArticleScreen;
+
+const styles = StyleSheet.create({});
